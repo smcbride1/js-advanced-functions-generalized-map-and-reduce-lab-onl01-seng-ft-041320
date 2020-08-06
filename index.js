8da79ce520e6bc8b294fddc21b70e558e553dcae
Stephen McBride <stevemcbride3@gmail.com>
2020-08-05 21:27:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your functions here
+function map(a, func) {
+    let newArray = [];
+    for (let e of a) {
+        newArray.push(func(e));
+    }
+    return newArray;
+}
+
+function reduce(sourceArray, func, startingPoint) {
+    let memo;
+    let i;
+    if (!startingPoint) {
+        memo = sourceArray[0];
+        i = 1;
+    } else {
+        memo = startingPoint;
+        i = 0;
+    }
+    for (; i < sourceArray.length; i++) {
+        memo = func(sourceArray[i], memo);
+    }
+    return memo;
+}
